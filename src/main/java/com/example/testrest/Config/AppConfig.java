012d629d5eb70e7a11448df996b1d721d5aaaffe
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-09-07 17:48:39 -0500

    Added Config Package

--- src/main/java/com/example/testrest/Config/AppConfig.java
+++ src/main/java/com/example/testrest/Config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.testrest;
+package com.example.testrest.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/testrest/Controller/RestTemplateController.java
+++ src/main/java/com/example/testrest/Controller/RestTemplateController.java
@@ -4,6 +4,7 @@ import com.example.testrest.Model.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,19 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class RestTemplateController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${users.url}")
+    String url;
+
+
+
+
+
     @Autowired
     RestTemplate restTemplate;
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
 
     @GetMapping("/users")
@@ -23,7 +33,8 @@ public class RestTemplateController {
 
 //        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/home/users").build().toUri();
 //        Sends an HTTP GET request, returning a ResponseEntity containing an object mapped from the response body
-        ResponseEntity<String> response =  restTemplate.getForEntity("http://localhost:8080/users", String.class);
+        ResponseEntity<String> response =  restTemplate.getForEntity(url, String.class);
+
         logger.info("Fetch : " + response.getBody() + " " + response.getStatusCode() + " " + response.getHeaders().getDate());
         return response.getBody();
 //        return   restTemplate.getForObject("http://localhost:8080/home/users", String.class);
@@ -32,7 +43,7 @@ public class RestTemplateController {
     @PostMapping("/users")
     public String  postUsers(@RequestBody Users users){
         HttpEntity<Users> request = new HttpEntity<>(users);
-        return restTemplate.postForObject("http://localhost:8080/users", request, String.class);
+        return restTemplate.postForObject(url, request, String.class);
     }
 
 
@@ -41,13 +52,13 @@ public class RestTemplateController {
         HttpEntity<Users> request = new HttpEntity<>(users);
 //        restTemplate.put("", Users.class, id);
         // Put Method Return Void
-         restTemplate.put("http://localhost:8080/users/{id}", request, id);
+         restTemplate.put( url +"/{id}", request, id);
     }
 
     @DeleteMapping("/users/{id}")
     public void   deleteUser(@PathVariable int id){
 
-        restTemplate.delete("http://localhost:8080/users/{id}", id);
+        restTemplate.delete(url + "/{id}", id);
     }
 
 
